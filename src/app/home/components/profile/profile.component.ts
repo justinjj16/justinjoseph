8ef3f1259c30229a9ffa19ee4122a48c965fa248
Justin Joseph <mailtojustinabin@gmail.com>
2021-02-14 23:49:24 +0530

    deploymentMistackFix

--- src/app/home/components/profile/profile.component.ts
+++ src/app/home/components/profile/profile.component.ts
@@ -22,7 +22,7 @@ export class ProfileComponent implements OnInit {
     window.open('https://gitlab.com/justinjoseph08', "_blank")
   }
   gitHub() {
-    window.open('https://github.com/justinjj08', "_blank");
+    window.open('https://github.com/justinjj16', "_blank");
   }
   linkedIn() {
     window.open('https://www.linkedin.com/in/justinjj16/', "_blank")
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:'https://script.google.com/macros/s/AKfycbxnEfk16ttETbOa4i4_bfNxGbK8jpenQoROC07znA/exec'
 };
 
 /*
